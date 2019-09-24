a0e30ee50ad1ff1002ebcf60905da4aa2618daf6
Student <farnazardenti@gmail.com>
2019-09-24 14:31:01 +0100

    changed test so its testing behaviour not state

--- tests/transaction.test.js
+++ tests/transaction.test.js
@@ -5,16 +5,16 @@ beforeEach(() => {
 });
 
 test('balance is 0', () => {
-  expect(transaction._balance).toEqual(0);
+  expect(transaction.balance).toEqual(0);
 });
 
-test('adds credit of 1000 on date "10/01/2012"', () => {
+test('adds credit of 1000 on date "10/01/2012" with balance 0', () => {
   transaction.deposit("10/01/2012", 1000)
   expect(transaction.balance).toEqual(1000);
 });
 
 
-test('withdraws 500 on date "11/01/2012"', () => {
+test('withdraws 500 on date "11/01/2012" with balance of 1000', () => {
   transaction.deposit("10/01/2012", 1000)
   transaction.withdraw("11/01/2012", 500)
   expect(transaction.balance).toEqual(500);
