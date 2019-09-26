8c82b63fc8eb46ce60dac5b4f0e616b754758166
Student <farnazardenti@gmail.com>
2019-09-26 09:51:48 +0100

    Implemented function to format date

--- tests/transaction.test.js
+++ tests/transaction.test.js
@@ -1,9 +1,15 @@
 const Transaction = require('../lib/transaction');
+const MockDate = require('mockdate');
 
 beforeEach(() => {
   transaction = new Transaction();
 });
 
-test('returns empty array', () => {
+test('returns transaction log', () => {
   expect(transaction.createLog()).toEqual({});
+});
+
+test('formats date to mm/dd/yyyy', () => {
+  MockDate.set('09-24-2019');
+  expect(transaction.dateFormatter()).toEqual('09/24/2019');
 });
